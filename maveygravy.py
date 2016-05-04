aff1d1473e51631ac03949cc1e83f6ab6a3d42e9
Mavey Ma <mma@csumb.edu>
2016-05-04 14:36:35 -0700

    Resize and Display Photos

--- maveygravy.py
+++ maveygravy.py
@@ -7,7 +7,8 @@ Earth + Fire == Lava, Magma (4
 Earth + Air == Opposites (5
 Fire + Air == Lightning, Electricity (6
 """
-#LATER, NEED TO OPEN WINDOW TO DISPLAY AN IMAGE
+#DISPLAY IMAGE, RESIZE IMAGE
+import PIL
 from PIL import Image 
 #========================================
 #SUMMARY: Combines two elements.
@@ -21,54 +22,75 @@ def combine(firstElement, secondElement):
     if ((firstElement == "Water" and secondElement == "Earth") or 
         (firstElement == "Earth" and secondElement == "Water")):
         print "Water + Earth == Mud, Clay!"
-        #OPEN THE PICTURE, STORE IN mud
-        mud = Image.open("mud.jpg")
-        #OPENS IMAGE APP AND DISPLAYS PHOTO
-        mud.show()
+        displayImage(resizePhoto(500, "mud.jpg"))
     #Water + Fire == Opposites
     elif ((firstElement == "Water" and secondElement == "Fire") or 
              (firstElement == "Fire" and secondElement == "Water")):
         print "Water + Fire == Opposites!"
-        waterFire = Image.open("waterFire.png")
-        waterFire.show() 
+        displayImage(resizePhoto(500, "waterFire.png"))
     #Water + Air == Ice, Frost
     elif ((firstElement == "Water" and secondElement == "Air") or 
              (firstElement == "Air" and secondElement == "Water")):
         print "Water + Air == Ice, Frost!"
-        ice = Image.open("ice.jpg")
-        ice.show() 
+        displayImage(resizePhoto(500, "ice.jpg"))
     #Earth + Fire == Lava, Magma
     elif ((firstElement == "Earth" and secondElement == "Fire") or 
              (firstElement == "Fire" and secondElement == "Earth")):
         print "Earth + Fire == Lava, Magma!"
-        lava = Image.open("lava.jpg")
-        lava.show()
+        displayImage(resizePhoto(500, "lava.jpg"))
     #Earth + Air == Opposites
     elif ((firstElement == "Earth" and secondElement == "Air") or 
              (firstElement == "Air" and secondElement == "Earth")):
         print "Earth + Air == Opposites!"
-        earthAir = Image.open("earthAir.jpg")
-        earthAir.show()
+        displayImage(resizePhoto(500, "earthAir.jpg"))
     #Fire + Air == Lightning, Electricity
     elif ((firstElement == "Air" and secondElement == "Fire") or 
              (firstElement == "Fire" and secondElement == "Air")):
         print "Air + Fire == Lightning, Electricity!"
-        lightning = Image.open("lightning.jpg")
-        lightning.show()
+        displayImage(resizePhoto(400, "lightning.jpg"))
     #None of the above.
     else:
         print "Error! Code shouldn't reach here."
-        lion = Image.open("lion.jpg")
-        lion.show()
+        displayImage(resizePhoto(500, "lion.jpg"))
     return
 #========================================
+def displayImage(fileName):
+    #OPEN THE PICTURE, STORE IN photo
+    photo = Image.open(fileName)
+    #OPENS IMAGE APP AND DISPLAYS PHOTO
+    photo.show()
+    return True;
+
+def resizePhoto(baseWidth, fileName):
+    photo = Image.open(fileName)
+    wPercent = ( baseWidth / float(photo.size[0]) )
+    hSize = int( (float(photo.size[1]) * float(wPercent)) )
+    photo = photo.resize( (baseWidth, hSize), PIL.Image.ANTIALIAS)
+    newfileName = "resized_" + fileName
+    photo.save(newfileName)
+    return newfileName
+
+"""
+lion = Image.open("lion.jpg")
+wPercent = ( baseWidth / float(lion.size[0]) )
+hSize = int( (float(lion.size[1]) * float(wPercent)) )
+lion = lion.resize( (baseWidth, hSize), PIL.Image.ANTIALIAS)
+lion.save("resizedLion.jpg")
+displayImage("resizedLion.jpg")
+displayImage(resizePhoto(500, "lion.jpg"))
+"""
+
+
+
+
+
 
 #combine("Water", "Earth")
 #combine("Water", "Fire")
 #combine("Water", "Air")
 #combine("Earth", "Fire")
 #combine("Earth", "Air")
-#combine("Fire", "Air")
+combine("Fire", "Air")
 
 
 """
