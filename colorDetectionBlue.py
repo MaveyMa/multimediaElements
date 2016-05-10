8010259ad8e10262d61e945611110eede63353a7
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-05-09 21:14:09 -0700

    We are now able to detect Blue, red, yellow and green.  Also set a string variable called element to be set to a different value depending on color

--- colorDetectionBlue.py
+++ colorDetectionBlue.py
@@ -4,15 +4,16 @@ import cv2
 def getThresholdBlueImage(im):
     newim = cv.CloneImage(im)
     cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
-
+    
     imgFile = cv2.imread('vive.jpg')
 
     hsv=cv.CreateImage(cv.GetSize(im), 8, 3)
     cv.CvtColor(newim, hsv, cv.CV_BGR2HSV) # Convert image to HSV
     imThreshed = cv.CreateImage(cv.GetSize(im), 8, 1)
     #Do the threshold on the hsv image, with the right range for the blue color
-    cv.InRangeS(hsv, cv.Scalar(40, 100, 100), cv.Scalar(125, 200, 200), imThreshed)
+    cv.InRangeS(hsv, cv.Scalar(120, 100, 100), cv.Scalar(125, 200, 200), imThreshed)
     del hsv
+
     return imThreshed
 
 '''
