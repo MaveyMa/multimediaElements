987595406aae4b095d817f51787286890221da11
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-05-09 20:07:43 -0700

    It's now able to look for two colors.  Image will not appear until both colors are present at the same time

--- colorDetectionBlue.py
+++ colorDetectionBlue.py
@@ -1,7 +1,7 @@
-from colorDetectionBlue import getThresholdBlueImage
+
 import cv2.cv as cv
 import cv2
-def getThresholdImage(im):
+def getThresholdBlueImage(im):
     newim = cv.CloneImage(im)
     cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
 
@@ -10,12 +10,12 @@ def getThresholdImage(im):
     hsv=cv.CreateImage(cv.GetSize(im), 8, 3)
     cv.CvtColor(newim, hsv, cv.CV_BGR2HSV) # Convert image to HSV
     imThreshed = cv.CreateImage(cv.GetSize(im), 8, 1)
-    #Do the threshold on the hsv image, with the right range for the yellow color
-    cv.InRangeS(hsv, cv.Scalar(20, 100, 100), cv.Scalar(30, 255, 255), imThreshed)
+    #Do the threshold on the hsv image, with the right range for the blue color
+    cv.InRangeS(hsv, cv.Scalar(40, 100, 100), cv.Scalar(125, 200, 200), imThreshed)
     del hsv
     return imThreshed
 
-
+'''
 capture = cv.CaptureFromCAM(0)
 
 cv.NamedWindow("video")
@@ -24,8 +24,6 @@ cv.NamedWindow("thresh")
 tmp = cv.QueryFrame(capture)
 imgScribble = cv.CreateImage(cv.GetSize(tmp), 8, 3) #Image that will contain lines
 
-posx = 0
-posy = 0
 bposx = 0
 bposy = 0
 imgFile = cv2.imread('vive.jpg') #500 x 281
@@ -33,29 +31,16 @@ size = (500, 281)
 
 while True:
     frame = cv.QueryFrame(capture)
-    imgBlueTresh = getThresholdBlueImage(frame)
-    imgYellowTresh = getThresholdImage(frame) #Apply the threshold function
 
-    moments = cv.Moments(cv.GetMat(imgYellowTresh),1)
-    moment10 = cv.GetSpatialMoment(moments, 1, 0)
-    moment01 = cv.GetSpatialMoment(moments, 0, 1)
-    area = cv.GetCentralMoment(moments, 0, 0) #Get the center
+    imgBlueTresh = getThresholdBlueImage(frame) #Apply the threshold function
 
     bmoments = cv.Moments(cv.GetMat(imgBlueTresh),1)
     bmoment10 = cv.GetSpatialMoment(bmoments, 1, 0)
     bmoment01 = cv.GetSpatialMoment(bmoments, 0, 1)
     barea = cv.GetCentralMoment(bmoments, 0, 0) #Get the center
 
-    lastx = posx
-    lasty = posy
     blastx = bposx
     blasty = bposy
-    if area == 0:
-        posx = 0
-        posy = 0
-    else:
-        posx = moment10/area
-        posy = moment01/area
     if barea == 0:
         bposx = 0
         bposy = 0
@@ -63,12 +48,14 @@ while True:
     else:
         bposx = bmoment10/barea
         bposy = bmoment01/barea
-    if lastx > 0 and lasty > 0 and posx > 0 and posy > 0 and blastx > 0 and blasty > 0 and bposx > 0 and bposy > 0: #Mean we have received coordinates to print
+
+    if blastx > 0 and blasty > 0 and bposx > 0 and bposy > 0: #Mean we have received coordinates to print
         #Draw the line
 
-        #When a yellow object is in the frame this image will appear
+        #When a blue object is in the frame this image will appear
         cv2.imshow('dst_rt', imgFile)
-
+        x_offset=y_offset=50
+        print 'blue'
         #frame[y_offset:y_offset+size.shape[0], x_offset:x_offset+size.shape[1]] = size
         #cv.Line(imgScribble, (int(posx),int(posy)), (int(lastx),int(lasty)), cv.Scalar(0, 255,255),3,1)
     else:
@@ -78,10 +65,11 @@ while True:
     cv.Add(frame, imgScribble, frame)
 
     cv.ShowImage("video", frame)
-    #cv.ShowImage("thresh", imgYellowTresh)
+    #cv.ShowImage("thresh", imgBlueTresh)
 
     c=cv.WaitKey(1)
     if c==27 or c==1048603: #Break if user enters 'Esc'.
         break
     elif c== 1048690: # 'r' for reset
         cv.Zero(imgScribble)
+'''
