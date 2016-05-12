d48398d13f83ee2b1e3cbf7bf133923d0c19cbfd
Mavey Ma <mma@csumb.edu>
2016-05-11 17:58:35 -0700

    Try color

--- colorDetection.py
+++ colorDetection.py
@@ -1,8 +1,6 @@
-from colorDetectionBlue import getThresholdBlueImage
+from displayImages import combine
 import cv2.cv as cv
 import cv2
-import maveygravy #IMPORT MAVEY'S PYTHON FILE
-
 def getThresholdImage(im):
     newim = cv.CloneImage(im)
     cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
@@ -17,6 +15,50 @@ def getThresholdImage(im):
     del hsv
     return imThreshed
 
+def getThresholdGreenImage(im):
+    newim = cv.CloneImage(im)
+    cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
+
+    imgFile = cv2.imread('vive.jpg')
+
+    hsv=cv.CreateImage(cv.GetSize(im), 8, 3)
+    cv.CvtColor(newim, hsv, cv.CV_BGR2HSV) # Convert image to HSV
+    imThreshed = cv.CreateImage(cv.GetSize(im), 8, 1)
+    #Do the threshold on the hsv image, with the right range for the green color
+    cv.InRangeS(hsv, cv.Scalar(60, 100, 100), cv.Scalar(125, 200, 200), imThreshed)
+    del hsv
+    return imThreshed
+
+def getThresholdBlueImage(im):
+    newim = cv.CloneImage(im)
+    cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
+
+    imgFile = cv2.imread('vive.jpg')
+
+    hsv=cv.CreateImage(cv.GetSize(im), 8, 3)
+    cv.CvtColor(newim, hsv, cv.CV_BGR2HSV) # Convert image to HSV
+    imThreshed = cv.CreateImage(cv.GetSize(im), 8, 1)
+    #Do the threshold on the hsv image, with the right range for the blue color
+    cv.InRangeS(hsv, cv.Scalar(120, 100, 100), cv.Scalar(125, 200, 200), imThreshed)
+    del hsv
+
+    return imThreshed
+
+def getThresholdRedImage(im):
+    newim = cv.CloneImage(im)
+    cv.Smooth(newim, newim, cv.CV_BLUR,12) #Remove noise
+
+    imgFile = cv2.imread('vive.jpg')
+
+    hsv=cv.CreateImage(cv.GetSize(im), 8, 3)
+    cv.CvtColor(newim, hsv, cv.CV_BGR2HSV) # Convert image to HSV
+    imThreshed = cv.CreateImage(cv.GetSize(im), 8, 1)
+    #Do the threshold on the hsv image, with the right range for the red color
+    cv.InRangeS(hsv, cv.Scalar(0, 100, 100), cv.Scalar(0, 200, 200), imThreshed)
+    del hsv
+    return imThreshed
+
+
 capture = cv.CaptureFromCAM(0)
 
 cv.NamedWindow("video")
@@ -24,18 +66,26 @@ cv.NamedWindow("thresh")
 
 tmp = cv.QueryFrame(capture)
 imgScribble = cv.CreateImage(cv.GetSize(tmp), 8, 3) #Image that will contain lines
-
+#yellow
 posx = 0
 posy = 0
+#blue
 bposx = 0
 bposy = 0
+#red
+rposx = 0
+rposy = 0
+
 imgFile = cv2.imread('vive.jpg') #500 x 281
 size = (500, 281)
-element = ""
+element1 = ""
+element2 = ""
 while True:
     frame = cv.QueryFrame(capture)
     imgBlueTresh = getThresholdBlueImage(frame)
     imgYellowTresh = getThresholdImage(frame) #Apply the threshold function
+    imgGreenTresh = getThresholdGreenImage(frame)
+    imgRedTresh = getThresholdRedImage(frame)
 
     moments = cv.Moments(cv.GetMat(imgYellowTresh),1)
     moment10 = cv.GetSpatialMoment(moments, 1, 0)
@@ -47,44 +97,65 @@ while True:
     bmoment01 = cv.GetSpatialMoment(bmoments, 0, 1)
     barea = cv.GetCentralMoment(bmoments, 0, 0) #Get the center
 
-    lastx = posx #yellow
+    rmoments = cv.Moments(cv.GetMat(imgRedTresh),1)
+    rmoment10 = cv.GetSpatialMoment(rmoments, 1, 0)
+    rmoment01 = cv.GetSpatialMoment(rmoments, 0, 1)
+    rarea = cv.GetCentralMoment(rmoments, 0, 0) #Get the center
+
+    lastx = posx
     lasty = posy
-    blastx = bposx #blue
+
+    blastx = bposx
     blasty = bposy
+
+    rlastx = rposx
+    rlasty = rposy
+
+
     if area == 0: #yellow
         posx = 0
         posy = 0
     else:
         posx = moment10/area
         posy = moment01/area
-    if barea == 0: #blue
+
+    #blue
+    if barea == 0:
         bposx = 0
         bposy = 0
-
     else:
         bposx = bmoment10/barea
         bposy = bmoment01/barea
-    #IF CAMERA SEES YELLOW, IT'S AIR. DISPLAY AIR.
-    if lastx > 0 and lasty > 0 and posx > 0 and posy > 0: #Means we have received coordinates to print
-        #Draw the line
 
-        #When a yellow object is in the frame this image will appear
-        cv2.imshow('dst_rt', imgFile)
-        element = "Air"
-        print element
-        #frame[y_offset:y_offset+size.shape[0], x_offset:x_offset+size.shape[1]] = size
-        #cv.Line(imgScribble, (int(posx),int(posy)), (int(lastx),int(lasty)), cv.Scalar(0, 255,255),3,1)
-    #IF CAMERA SEES BLUE, IT'S WATER. DISPLAY BLUE.
-    elif blastx > 0 and blasty > 0 and bposx > 0 and bposy > 0:
-        element = "Water"
-        print element
-    #TODO:IF CAMERA SEES RED, IT'S FIRE. DISPLAY FIRE.
-    #RIGHT NOW THIS IS AN ELSE STATEMENT. BUT TODO: IF CAMERA SEES GREEN, IT'S EARTH. DISPLAY EARTH.
+    #red
+    if rarea == 0:
+        rposx = 0
+        rposy = 0
     else:
-        element = "Earth"
-        print element
+        rposx = rmoment10/rarea
+        rposy = rmoment01/rarea
+
+
+    if lastx > 0 and lasty > 0 and posx > 0 and posy > 0: #Mean we have received coordinates to print
+    #if there is a yellow object present
+        #cv2.imshow('dst_rt', imgFile)
+        element1 = "Air"
+    else:
+        element1 = ""
+    if blastx > 0 and blasty > 0 and bposx > 0 and bposy > 0: #Mean we have received coordinates to print
+    #if there is a yellow object present
+        #cv2.imshow('dst_rt', imgFile)
+        element2 = "Water"
+    else:
+        element2 = ""
+
+    combine(element1, element2)
+    print element1
+    print element2
+
+
         #image will disappear if the color object is not present
-        cv.DestroyWindow('dst_rt')
+        #cv.DestroyWindow('dst_rt')
     #Add the frame and the line image to see lines on the webcam frame
     cv.Add(frame, imgScribble, frame)
 
